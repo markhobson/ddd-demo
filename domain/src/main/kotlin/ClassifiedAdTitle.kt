9e723daf09e27b1ab6693a74709a76b5bb27e243
Mark Hobson <markhobson@gmail.com>
2020-09-22 15:34:13 +0100

    Introduce value type for classified ad title

---
+++ domain/src/main/kotlin/ClassifiedAdTitle.kt
@@ -0,0 +1,16 @@
+package marketplace.domain
+
+import java.lang.IllegalArgumentException
+
+// Data class constructor cannot be private
+data class ClassifiedAdTitle(private val value: String) {
+    init {
+        if (value.length > 100) {
+            throw IllegalArgumentException("Title cannot be longer than 100 characters")
+        }
+    }
+
+    companion object {
+        fun fromString(title: String): ClassifiedAdTitle = ClassifiedAdTitle(title)
+    }
+}
