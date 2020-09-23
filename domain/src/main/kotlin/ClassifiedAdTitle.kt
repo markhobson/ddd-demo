4d672f06275fe56ee02d6150f3520130cfd6fbca
Mark Hobson <markhobson@gmail.com>
2020-09-23 09:42:46 +0100

    Add missing ad title from HTML factory method

--- domain/src/main/kotlin/ClassifiedAdTitle.kt
+++ domain/src/main/kotlin/ClassifiedAdTitle.kt
@@ -9,6 +9,19 @@ data class ClassifiedAdTitle internal constructor(val value: String) {
             return ClassifiedAdTitle(title)
         }
 
+        fun fromHtml(htmlTitle: String): ClassifiedAdTitle {
+            val supportedTagsReplaced = htmlTitle
+                .replace("<i>", "*")
+                .replace("</i>", "*")
+                .replace("<b>", "*")
+                .replace("</b>", "**")
+
+            val value = supportedTagsReplaced.replace(Regex("<.*?>"), "")
+            checkValidity(value)
+
+            return ClassifiedAdTitle(value)
+        }
+
         private fun checkValidity(value: String) {
             if (value.length > 100) {
                 throw IllegalArgumentException("Title cannot be longer than 100 characters")
